57c1876b497fbf646fb0c16f9fb8c7377f73cbf5
LorenzoPerotto <lorenzoperotto05@gmail.com>
2026-01-29 15:32:42 +0100

    Update Itinerary.js

--- src/models/Itinerary.js
+++ src/models/Itinerary.js
@@ -4,13 +4,13 @@ const itinerarySchema = new mongoose.Schema(
   {
     nome: { type: String, required: true, unique: true },
     tipologia: { type: String },
-    tempo: { type: Number },
-    budget: { type: Number },
+    tempo: { type: Number, default: 0 },
+    budget: { type: Number, default 0 },
     descrizione: { type: String },
     creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
     activities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Activity' }],
     numPersone: { type: Number, default: 0 },
-    etaMedia: { type: Number },
+    etaMedia: { type: Number, default: 0 },
     orarioAffluenza: { type: String }
   },
   { timestamps: true }
